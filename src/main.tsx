aaf6ccc1da44d4280e6b046e5cd4e0ad276039c0
sqlbipro <sqlbipro@gmail.com>
2025-05-18 17:58:23 -0700

    hashrouter change

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { HashRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
+
+
+
 // Remove dark mode class addition
-createRoot(document.getElementById("root")!).render(<App />);
+//createRoot(document.getElementById("root")!).render(<App />);
